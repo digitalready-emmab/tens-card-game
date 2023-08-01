2fa100552151ad5b5cc166dab0d66b3bdf54b20b
digitalready-emmab <emma.b@digitalready.org>
2023-08-01 16:13:47 -0400

    Co-authored-by: digitalready-veronica <digitalready-veronica@users.noreply.github.com> Co-authored-by: digitalready-stephanieha <digitalready-stephanieha@users.noreply.github.com> Co-authored-by: bernisha1 <bernisha1@users.noreply.github.com> Co-authored-by: digitalready-joi <digitalready-joi@users.noreply.github.com>

--- testing.py
+++ testing.py
@@ -1,4 +1,32 @@
 from card import Card
 import random
 
-print (Card.new_deck()[2:10])
+
+turn_number = 1
+whos_turn = ''
+print(whos_turn)
+
+class Player():
+    def __init__(self,name,hand):
+        self.name = name
+        self.hand = hand
+
+class User(Player):
+    pass
+
+player = Player(input("What is your name? "), Card.new_deck()[0:5])
+print(player.__dict__)
+
+class Computer(Player):
+    def __init__(self,name,hand):
+        self.name = name
+        self.hand = hand
+computer = Computer("Computer", Card.new_deck()[6:10])
+print(computer.__dict__)
+
+def player_turn():
+    if turn_number % 2 == 0:
+        whos_turn = 'player'
+    else:
+        whos_turn = 'computer'
+
